1a9ae186dd47d4d33f5dac387c7106262cea849c
Keegan Bowen <keegan.bowen@gmail.com>
2015-02-22 21:25:45 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p /usr/local/scripts 2> /dev/null
+mkdir -p /usr/local/lib 2> /dev/null
+cp ./forcematrix.py /usr/local/scripts/forcematrix.py
+chmod 777 /usr/local/lib/forcematrix.in
